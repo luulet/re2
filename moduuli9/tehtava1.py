ec982f9c285e90982cf8a712102d0c6e54c3e5a1
luulee <luulee@github.com>
2025-03-19 09:58:51 +0200

    moduuli9 aloitus

---
+++ moduuli9/tehtava1.py
@@ -0,0 +1,18 @@
+
+class Auto:
+    def __init__(self, rekisteritunnus, huippunopeus, nopeus, matka):
+        self.rekisteritunnus = rekisteritunnus
+        self.huippunopeus = huippunopeus
+        self.nopeus = nopeus
+        self.matka = matka
+
+
+def main(self, kerrat):
+    auto1 = Auto("ABC-123","142 km/h", 0, 0)
+    for i in range(kerrat):
+        print(str(self.rekisteritunnus), str(self.huippunopeus), str(self.nopeus), str(self.matka))
+
+
+    return
+auto1 = "0"
+main(auto1, 1)
