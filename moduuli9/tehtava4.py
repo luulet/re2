9281c4b6794507139ab88ece6b6d6175bd2dbe9a
luulee <luulee@github.com>
2025-03-19 12:03:29 +0200

    tehtava 4 vaiheessa

--- moduuli9/tehtava4.py
+++ moduuli9/tehtava4.py
@@ -11,35 +11,39 @@ def main():
             self.matka = matka
 
         def accelerate(self, muutos):
-            for i in muutos:
+            self.nopeus += muutos
 
-                self.nopeus += i
-
-                if self.nopeus > self.huippunopeus:
+            if self.nopeus > self.huippunopeus:
                     self.nopeus = self.huippunopeus
 
-                elif self.nopeus <= 0:
+            elif self.nopeus <= 0:
                     self.nopeus = 0
 
 
-                if auto1.nopeus >= 142:
-                    print(f'Auton tämänhetkinen nopeus: {auto1.nopeus} km/h')
-
         def kulje(self, time):
             self.matka += time * self.nopeus
-    muutos =(30,70,50,-200)
+    muutos =(int(random.randint(-10,15)))
     auto1 = Auto("ABC-123",142, 60, 0)
     autot = []
     for i in range(10):
         kilpa_auto = Auto(f'ABC-{i+1}', random.randint(100,200),0,0)
         autot.append(kilpa_auto)
 
-    print(autot)
+    for kilpa_auto in autot:
+        print(kilpa_auto.rekisteritunnus)
+        print(kilpa_auto.huippunopeus)
+
+    while True:
+        kilpa_auto.kulje(1)
+        kilpa_auto.accelerate(muutos)
+
+        for kilpa_auto in autot:
+            print(kilpa_auto.matka)
    # print("Auton ominaisuudet:")
    # print(f'{auto1.rekisteritunnus}, {auto1.huippunopeus}, {auto1.nopeus} km/h, {auto1.matka} km')
     # auto1.accelerate(muutos)
 
-    return
+
 
 main()
 
